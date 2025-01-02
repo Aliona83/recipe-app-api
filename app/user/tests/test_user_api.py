363d959336919e6d129944c98f80b0ff56593cdb
Aliona Janusko <ajanusko@em3.ie>
2025-01-02 15:13:34 +0000

    create recipe app and testing

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -151,11 +151,11 @@ class PrivateUserApiTests(TestCase):
             'email': self.user.email,
         })
 
-    def test_post_me_not_allowed(self):
-        """Test POST is not allowed for the me endpoint."""
-        res = self.client.post(ME_URL, {})
+    # def test_post_me_not_allowed(self):
+    #     """Test POST is not allowed for the me endpoint."""
+    #     res = self.client.post(ME_URL, {})
 
-        self.assertEqual(res.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+    #     self.assertEqual(res.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
 
     def test_update_user_profile(self):
         """Test updating the user profile for the authenticated user."""
